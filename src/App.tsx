06b18fa8fe384571c11a4705d60c926e5c6d90be
Mateusz Kluska <mateuszkluska7@gmail.com>
2023-10-04 20:49:21 +0200

    feat: add lazy loading, chart register, error boundary, loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,42 @@
 import { Suspense } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import {
+  BarElement,
+  CategoryScale,
+  Chart as ChartJS,
+  Legend,
+  LinearScale,
+  Title,
+  Tooltip,
+} from 'chart.js';
+
+import ErrorBoundary from './modules/ErrorBoundary/ErrorBoundary';
+
+import { Error404 } from './pages/Error404/Error404';
+
+import { lazy } from 'react';
 import './App.scss';
+import { Loader } from './common/Loader/Loader';
 
-import { Graph } from './pages/Graph';
-import { Dashobard } from './pages/Dashobard';
+const LazyGraph = lazy(() => import('./pages/Graph'));
+const LazyDashborad = lazy(() => import('./pages/Dashobard'));
 
-function App() {
-  return (
-    <>
+ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
+
+const App = () => (
+  <>
+    <ErrorBoundary>
       <BrowserRouter>
-        <Suspense fallback='Loading...'>
+        <Suspense fallback={<Loader />}>
           <Routes>
-            <Route path='/' element={<Graph />} />
-            <Route path='/dashboard' element={<Dashobard />} />
+            <Route element={<Error404 />} path='*' />
+            <Route element={<LazyGraph />} path='/' />
+            <Route element={<LazyDashborad />} path='/dashboard' />
           </Routes>
         </Suspense>
       </BrowserRouter>
-    </>
-  );
-}
+    </ErrorBoundary>
+  </>
+);
 
 export default App;
