17750ce2a8cac1487ec6b004ffa9ed1172250447
Mateusz Kluska <mateuszkluska7@gmail.com>
2023-10-04 20:56:39 +0200

    feat: remove theme provider, correction of a typo

--- src/modules/Dashboard/components/ObjectsTable/ObjectsTable.tsx
+++ src/modules/Dashboard/components/ObjectsTable/ObjectsTable.tsx
@@ -1,21 +1,21 @@
-import styles from './ObjectTable.module.scss';
 import { useMemo } from 'react';
 import { MaterialReactTable, type MRT_ColumnDef } from 'material-react-table';
-import { createTheme, ThemeProvider } from '@mui/material';
 import { MRT_Localization_PL } from 'material-react-table/locales/pl';
 import { ManagedObjects, objects } from 'src/modules/Dashboard/constants/objects';
+
+import styles from './ObjectTable.module.scss';
 
 export const ObjectsTable = () => {
   const columns = useMemo<MRT_ColumnDef<ManagedObjects>[]>(
     () => [
       {
         accessorKey: 'name',
-        header: 'Nazwa objektu',
+        header: 'Nazwa obiektu',
         size: 200,
       },
       {
         accessorKey: 'typeOfObject',
-        header: 'Typ objektu',
+        header: 'Typ obiektu',
         size: 200,
       },
       {
@@ -32,68 +32,15 @@ export const ObjectsTable = () => {
     [],
   );
 
-  const tableTheme = useMemo(
-    () =>
-      createTheme({
-        palette: {
-          mode: 'dark',
-          primary: {
-            main: '#f3f3f3',
-          },
-          secondary: {
-            main: '#f3f3f3',
-          },
-          info: {
-            main: '#D12727',
-          },
-          background: {
-            default: '#1d1d1d',
-          },
-        },
-        typography: {
-          button: {
-            textTransform: 'none',
-            fontSize: '1.2rem',
-          },
-        },
-        components: {
-          MuiTableHead: {
-            styleOverrides: {
-              root: {
-                backgroundColor: 'yellow',
-              },
-            },
-          },
-          MuiTooltip: {
-            styleOverrides: {
-              tooltip: {
-                fontSize: '1.1rem',
-              },
-            },
-          },
-          MuiSwitch: {
-            styleOverrides: {
-              thumb: {
-                color: '#137813',
-              },
-            },
-          },
-        },
-      }),
-    [],
-  );
-
   return (
     <div className={styles['table-container']}>
-      <ThemeProvider theme={tableTheme}>
-        <MaterialReactTable
-          enableColumnActions
-          enableColumnDragging={true}
-          localization={MRT_Localization_PL}
-          columns={columns}
-          data={objects}
-        />
-      </ThemeProvider>
+      <MaterialReactTable
+        enableColumnActions
+        columns={columns}
+        data={objects}
+        enableColumnDragging={true}
+        localization={MRT_Localization_PL}
+      />
     </div>
   );
 };
